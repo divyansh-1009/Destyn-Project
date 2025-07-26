da8640a2acbdb37d677845cadb553a7cc0d58f39
divyansh-1009 <anshmcs@gmail.com>
2025-07-27 04:44:51 +0530

    chore(ui): revamp user interface components and global styling
    - browser tab pr logo change krdia gaya hai guysssssss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ import { SessionProvider } from "next-auth/react";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/vercel.svg" type="image/svg+xml" />
+        <title>Destyn - Dil se Date tak...</title>
+      </head>
       <body>
         <SessionProvider>{children}</SessionProvider>
       </body>
